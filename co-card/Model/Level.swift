8bdeb80deddf0e607611ba53faaf3a9b1f0988e4
QiChen <shen24qichen@gmail.com>
2021-01-09 11:57:38 +0900

    Level ModelでLevelを管理

--- co-card/Model/Level.swift
+++ co-card/Model/Level.swift
@@ -7,3 +7,26 @@
 //
 
 import Foundation
+
+class Level{
+    
+    var level = Int()
+    
+    ///最初のレベル
+    func firstLevel() -> String {
+        level = 1
+        return CurrentLevel()
+    }
+    
+    ///レベルを1上げる
+    func nextLevel() -> String{
+        level += 1
+        return CurrentLevel()
+    }
+    
+    ///現在のレベルを返す
+    func CurrentLevel() -> String{
+        return "Level \(level)"
+    }
+    
+}
